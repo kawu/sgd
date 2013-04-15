d05c50a34d1855cfb6d59a4c9fafbe2d81de5c0c
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-04-15 18:07:06 +0200

    Add abstract dataset representation

--- Numeric/SGD.hs
+++ Numeric/SGD.hs
@@ -2,8 +2,8 @@
 
 -- | Stochastic gradient descent implementation using mutable
 -- vectors for efficient update of the parameters vector.
--- A user is provided with the immutable version of parameters vector
--- so he is able to compute the gradient outside the IO/ST monad.
+-- A user is provided with the immutable vector of parameters
+-- so he is able to compute the gradient outside of the IO monad.
 -- Currently only the Gaussian priors are implemented.
 --
 -- This is a preliminary version of the SGD library and API may change
@@ -12,22 +12,20 @@
 module Numeric.SGD
 ( SgdArgs (..)
 , sgdArgsDefault
-, Dataset
 , Para
 , sgd
-, sgdM
 , module Numeric.SGD.Grad
+, module Numeric.SGD.Dataset
 ) where
 
 import Control.Monad (forM_)
-import Control.Monad.ST (ST, runST)
 import qualified System.Random as R
-import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import qualified Control.Monad.Primitive as Prim
 
 import Numeric.SGD.Grad
+import Numeric.SGD.Dataset
 
 -- | SGD parameters controlling the learning process.
 data SgdArgs = SgdArgs
@@ -52,45 +50,23 @@ sgdArgsDefault = SgdArgs
     , gain0     = 1
     , tau       = 5 }
 
--- | Dataset with elements of x type.
-type Dataset x  = V.Vector x
-
 -- | Vector of parameters.
 type Para       = U.Vector Double 
 
 -- | Type synonym for mutable vector with Double values.
-type MVect m    = UM.MVector (Prim.PrimState m) Double
+type MVect      = UM.MVector (Prim.PrimState IO) Double
 
--- | Pure version of the stochastic gradient descent method.
-sgd :: SgdArgs              -- ^ SGD parameter values
-    -> (Para -> x -> Grad)  -- ^ Gradient for dataset element
-    -> Dataset x            -- ^ Dataset
-    -> Para                 -- ^ Starting point
-    -> Para                 -- ^ SGD result
-sgd sgdArgs mkGrad dataset x0 =
-    let dummy _ _ = return ()
-    in  runST $ sgdM sgdArgs dummy mkGrad dataset x0
-
--- | Monadic version of the stochastic gradient descent method.
+-- | A stochastic gradient descent method.
 -- A notification function can be used to provide user with
 -- information about the progress of the learning.
-{-# SPECIALIZE sgdM :: SgdArgs
-                    -> (Para -> Int -> IO ())
-                    -> (Para -> x -> Grad)
-                    -> Dataset x -> Para -> IO Para #-}
-{-# SPECIALIZE sgdM :: SgdArgs
-                    -> (Para -> Int -> ST s ())
-                    -> (Para -> x -> Grad)
-                    -> Dataset x -> Para -> ST s Para #-}
-sgdM
-    :: (Prim.PrimMonad m)
-    => SgdArgs              -- ^ SGD parameter values
-    -> (Para -> Int -> m ())    -- ^ Notification run every update
-    -> (Para -> x -> Grad)  -- ^ Gradient for dataset element
-    -> Dataset x            -- ^ Dataset
-    -> Para                 -- ^ Starting point
-    -> m Para               -- ^ SGD result
-sgdM SgdArgs{..} notify mkGrad dataset x0 = do
+sgd
+    :: SgdArgs                  -- ^ SGD parameter values
+    -> (Para -> Int -> IO ())   -- ^ Notification run every update
+    -> (Para -> x -> Grad)      -- ^ Gradient for dataset element
+    -> Dataset x                -- ^ Dataset
+    -> Para                     -- ^ Starting point
+    -> IO Para                  -- ^ SGD result
+sgd SgdArgs{..} notify mkGrad dataset x0 = do
     u <- UM.new (U.length x0)
     doIt u 0 (R.mkStdGen 0) =<< U.thaw x0
   where
@@ -99,7 +75,7 @@ sgdM SgdArgs{..} notify mkGrad dataset x0 = do
     -- | Number of completed iterations over the full dataset.
     done k
         = fromIntegral (k * batchSize)
-        / fromIntegral (V.length dataset) 
+        / fromIntegral (size dataset)
 
     doIt u k stdGen x
       | done k > iterNum = do
@@ -107,7 +83,7 @@ sgdM SgdArgs{..} notify mkGrad dataset x0 = do
         notify frozen k
         return frozen
       | otherwise = do
-        let (batch, stdGen') = sample stdGen batchSize dataset
+        (batch, stdGen') <- sample stdGen batchSize dataset
 
         -- Freeze mutable vector of parameters. The frozen version is
         -- then supplied to external mkGrad function provided by user.
@@ -124,9 +100,7 @@ sgdM SgdArgs{..} notify mkGrad dataset x0 = do
         doIt u (k+1) stdGen' x'
 
 -- | Add up all gradients and store results in normal domain.
-{-# SPECIALIZE addUp :: Grad -> MVect IO -> IO () #-}
-{-# SPECIALIZE addUp :: Grad -> MVect (ST s) -> ST s () #-}
-addUp :: Prim.PrimMonad m => Grad -> MVect m -> m ()
+addUp :: Grad -> MVect -> IO ()
 addUp grad v = do
     UM.set v 0
     forM_ (toList grad) $ \(i, x) -> do
@@ -134,9 +108,7 @@ addUp grad v = do
         UM.unsafeWrite v i (x + y)
 
 -- | Scale the vector by the given value.
-{-# SPECIALIZE scale :: Double -> MVect IO -> IO () #-}
-{-# SPECIALIZE scale :: Double -> MVect (ST s) -> ST s () #-}
-scale :: Prim.PrimMonad m => Double -> MVect m -> m ()
+scale :: Double -> MVect -> IO ()
 scale c v = do
     forM_ [0 .. UM.length v - 1] $ \i -> do
         y <- UM.unsafeRead v i
@@ -144,19 +116,9 @@ scale c v = do
 
 -- | Apply gradient to the parameters vector, that is add the first vector to
 -- the second one.
-{-# SPECIALIZE apply :: MVect IO -> MVect IO -> IO () #-}
-{-# SPECIALIZE apply :: MVect (ST s) -> MVect (ST s) -> ST s () #-}
-apply :: Prim.PrimMonad m => MVect m -> MVect m -> m ()
+apply :: MVect -> MVect -> IO ()
 apply w v = do 
     forM_ [0 .. UM.length v - 1] $ \i -> do
         x <- UM.unsafeRead v i
         y <- UM.unsafeRead w i
         UM.unsafeWrite v i (x + y)
-
-sample :: R.RandomGen g => g -> Int -> Dataset x -> ([x], g)
-sample g 0 _       = ([], g)
-sample g n dataset =
-    let (xs, g') = sample g (n-1) dataset
-        (i, g'') = R.next g'
-        x = dataset V.! (i `mod` V.length dataset)
-    in  (x:xs, g'')
---
+++ Numeric/SGD/Dataset.hs
@@ -0,0 +1,101 @@
+{-# LANGUAGE RecordWildCards #-}
+
+
+-- | Dataset abstraction.
+
+
+module Numeric.SGD.Dataset
+( 
+-- * Dataset
+  Dataset (..)
+-- * Reading
+, loadData
+, sample
+-- * Construction
+, withVect
+, withDisk
+, withData
+) where
+
+
+import           Control.Monad (forM_)
+import           Data.Binary (Binary, encodeFile, decodeFile)
+import           System.IO.Unsafe (unsafeInterleaveIO)
+import           System.IO.Temp (withTempDirectory)
+import           System.FilePath ((</>))
+import qualified System.Random as R
+import qualified Data.Vector as V
+import qualified Control.Monad.State.Strict as S
+
+
+-- | A dataset with elements of type @a@.
+data Dataset a = Dataset {
+    -- | A size of the dataset.
+      size      :: Int
+    -- | Get dataset element with a given index.  The set of indices
+    -- is of a {0, 1, .., size - 1} form.
+    , elemAt    :: Int -> IO a }
+
+
+-------------------------------------------
+-- Reading
+-------------------------------------------
+
+
+-- | Lazily load dataset from a disk.
+loadData :: Dataset a -> IO [a]
+loadData Dataset{..} = lazyMapM elemAt [0 .. size - 1]
+
+
+-- | A dataset sample of the given size.
+sample :: R.RandomGen g => g -> Int -> Dataset a -> IO ([a], g)
+sample g 0 _       = return ([], g)
+sample g n dataset = do
+    (xs, g') <- sample g (n-1) dataset
+    let (i, g'') = R.next g'
+    x <- dataset `elemAt` (i `mod` size dataset)
+    return (x:xs, g'')
+
+
+lazyMapM :: (a -> IO b) -> [a] -> IO [b]
+lazyMapM f (x:xs) = do
+    y <- f x
+    ys <- unsafeInterleaveIO $ lazyMapM f xs
+    return (y:ys)
+lazyMapM _ [] = return []
+
+
+-------------------------------------------
+-- Construction
+-------------------------------------------
+
+
+-- | Construct dataset from a vector of elements and run the
+-- given handler.
+withVect :: [a] -> (Dataset a -> IO b) -> IO b
+withVect xs handler =
+    handler dataset
+  where
+    v = V.fromList xs
+    dataset = Dataset
+        { size      = V.length v
+        , elemAt    = \k -> return (v V.! k) }
+
+
+-- | Construct dataset from a list of elements, store it on a disk
+-- and run the given handler.
+withDisk :: Binary a => [a] -> (Dataset a -> IO b) -> IO b
+withDisk xs handler = withTempDirectory "." "dataset" $ \tmpDir -> do
+    n <- flip S.execStateT 0 $ forM_ (zip xs [0 :: Int ..]) $ \(x, ix) -> do
+        S.lift $ encodeFile (tmpDir </> show ix) x
+        S.modify (+1)
+    let at ix = decodeFile (tmpDir </> show ix)
+    handler $ Dataset {size = n, elemAt = at}
+
+
+-- | Use disk or vector dataset representation depending on
+-- the given argument. 
+withData :: Binary a => Bool -> [a] -> (Dataset a -> IO b) -> IO b
+withData x = case x of
+    True    -> withDisk
+    False   -> withVect
